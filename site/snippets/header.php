c06ae86fc513dfe4fb54f866bf9bacf5c191575e
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-07-26 11:22:57 +1200

    Intellisense package added. header and footer php files added to snippets

---
+++ site/snippets/header.php
@@ -0,0 +1,22 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= $site->title() ?>
+    </title>
+
+    <?= css('assets/css/index.css') ?>
+
+</head>
+
+<header class="header">
+    <div>
+        <img src="content/home/PoisonFrogLogoLockup.png" alt="Frog foot illustration with text" />
+    </div>
+    <div>
+        <h2>Auckland, New zealand</h2>
+        <h2>hello@poisonfrog.com</h2>
+    </div>
+</header>
