27282d99bca9e764159b1e103a99e25287079cd4
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-07-26 10:53:06 +1200

    Added css file, added html to default fallback site template, added some file structure to content folder

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,18 @@
-<h1><?= $page->title() ?></h1>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $site->title() ?></title>
+
+    <?= css('assets/css/index.css') ?>
+    
+</head>
+<body>
+
+    <h1><?= $page->title() ?></h1>
+    <p> Hello World </p>
+
+</body>
+</html>
+
+
