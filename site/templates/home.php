b95d19e4e137899ccdafb567fcb3026528d75e8e
Deshan Mclachlan <deshan.mclachlan@developersinstitute.co.nz>
2023-07-28 12:08:28 +1200

    Home css updated to suit small screens, added a video embed url to reverie txt file as a test video

--- site/templates/home.php
+++ site/templates/home.php
@@ -4,30 +4,36 @@
     <ul class="project-list">
         <?php foreach ($page->children()->listed() as $item): ?>
             <li class="project-list-item">
-                <h2 class="project-title">
-                    <?= $item->title() ?>
-                </h2>
+                <div class="project-title-grid">
+                    <h2 class="project-title project-title-padding project-title-grid-item">
+                        <?= $item->title() ?>
+                    </h2>
+                    <p class="project-length project-length-padding project-length-grid-item">00:00:01:23</p>
+                </div>
                 <ul>
-                    <li class="project-camera-used">
-                        <p>
-                            <?= $item->CameraUsed() ?>
-                        </p>
+                    <li>
+                        <?= $item->VideoEmbedURL() ?>
                     </li>
-                    <li class="project-director">
-                        <h3 class="project-director-title">Director</h3>
-                        <p class="project-director-text">
+                    <li class="project-camera-used project-camera-used-padding">
+                        <h3>
+                            <?= $item->CameraUsed() ?>
+                        </h3>
+                    </li>
+                    <li class="project-director project-description-grid project-description-padding">
+                        <h3 class="project-director-title project-grid-item-a">Director:</h3>
+                        <p class="project-director-text project-grid-item-b">
                             <?= $item->Director() ?>
                         </p>
                     </li>
-                    <li class="project-assistant">
-                        <h3 class="project-assistant-title">Assistant</h3>
-                        <p class="project-assistant-text">
+                    <li class="project-assistant project-description-grid project-description-padding">
+                        <h3 class="project-assistant-title project-grid-item-a">Assistant:</h3>
+                        <p class="project-assistant-text project-grid-item-b">
                             <?= $item->Assistant() ?>
                         </p>
                     </li>
-                    <li class="project-lighting">
-                        <h3 class="project-lighting-title">Lighting</h3>
-                        <p class="project-lighting-text">
+                    <li class="project-lighting project-description-grid">
+                        <h3 class="project-lighting-title project-grid-item-a">Lighting:</h3>
+                        <p class="project-lighting-text project-grid-item-b">
                             <?= $item->Lighting() ?>
                         </p>
                     </li>
